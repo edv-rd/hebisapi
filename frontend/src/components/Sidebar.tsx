37c88348551a05fc707277d73714d5a2745b2391
Ed Code <edvard.ekstrom@gmail.com>
2025-09-05 17:06:02 +0200

    updated sidebar ui

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,30 +1,28 @@
 import categories from "../Categories";
 
 interface SidebarProps {
-  handleShownCategory: (type: string) => void; // Define a prop for handling entry hiding
+  handleShownCategory: (type: string) => void;
 }
 
 function Sidebar({ handleShownCategory }: SidebarProps) {
-  // Use a Set to keep track of unique category types
   const uniqueCategoryTypes = Array.from(
     new Set(categories.map((c) => c.type))
   );
 
   return (
-    <div className="flex flex-row gap-2 max-w-1">
-      <div className="w-auto">
+    <div className="flex flex-wrap gap-2 p-2 max-w-full">
+      <div>
         <button
-          className="text-white bg-blue-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          className="text-white bg-blue-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           onClick={() => handleShownCategory("")}
         >
           Visa alla
         </button>
       </div>
       {uniqueCategoryTypes.map((type) => (
-        <div className="w-auto" key={type}>
+        <div key={type}>
           <button
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            key={type}
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             onClick={() => handleShownCategory(type)}
           >
             {type}
